8e5bccfb8876e964f392f0ab0373c8a71b877879
K-Oxon <ko1011qfp@gmail.com>
2026-03-09 22:39:40 +0900

    Bump version from 0.2.0 to 0.3.0

--- src/estat_api_dlt_helper/__init__.py
+++ src/estat_api_dlt_helper/__init__.py
@@ -1,6 +1,6 @@
 """estat_api_dlt_helper - e-Stat API data loader using DLT."""
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 from .api.client import EstatApiClient
 from .config import DestinationConfig, EstatDltConfig, SourceConfig
